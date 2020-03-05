51d5877a32b3e164e4590f5a41338fbeb48d33b9
xiangguoshuai <851793249@qq.com>
2020-03-05 18:39:51 +0800

    创建

---
+++ spring-boot-user/src/main/java/org/spring/boot/user/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.spring.boot.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class HelloController {
+
+       @RequestMapping("/hello")
+       public String hello() {
+
+              return "helloworld2";
+
+       }
+
+}
