de89280a7a63d1528453416c89b07ee551c269c7
xiangguoshuai <851793249@qq.com>
2020-03-05 18:44:24 +0800

    提交

--- spring-boot-user/src/main/java/org/spring/boot/user/App.java
+++ spring-boot-user/src/main/java/org/spring/boot/user/App.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
-public class App{
+public class App{ 
 	public static void main(String[] args) { 
 		SpringApplication.run(App.class, args);
 	}
